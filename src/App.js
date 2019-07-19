16cb8edbaa517b3bac8e2d89eeed46b3753d2ca7
Heather Zurek <heather.zurek@gmail.com>
2019-07-19 13:18:30 -0700

    set up responsive drawer template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import ResponsiveDrawer from './Components/ResponsiveDrawer';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    // backgroundColor: '#02152e',
+    // color: '#319990',
+    // width: '996px',
+    // padding: '20px',
+    // margin: '0 auto',
+    // marginTop: '50px',
+    // textAlign: 'center',
+    // minHeight: 'calc(100vh - 84px)',
+    // margin: '-20px',
+    // flexDirection: 'row',
+    // alignItems: 'center',
+    // maxWidth: '996px',
+    // margin: '0 auto',
+    // justifyContent: 'space-between',
+
+// eslint-disable-next-line
+    // ['@media (max-width:780px)']: {
+    //   flexDirection: 'column',
+    //   alignItems: 'center',
+    // }
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.container}>
+      <ResponsiveDrawer />
+      <h1>2</h1>
+      <h1>3</h1>
+      <h1>4</h1>
+      <h1>5</h1>
+      <h1>6</h1>
+
     </div>
   );
 }
