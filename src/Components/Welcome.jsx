9202fe37ac5398c20a8c6e51c51ea593781b11ca
Heather Zurek <heather.zurek@gmail.com>
2019-07-26 12:44:44 -0700

    add hard coded resource links to welcome component

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -9,9 +9,17 @@ export default function Welcome(props) {
       }
     return (
     <div style={divStyles}>
-      <AnimatedTypingComponent />
-      {/* <h1>Welcome, Whats your name?</h1> */}
+      {/* <AnimatedTypingComponent /> */}
+      <h1>Welcome,</h1>
       <Intro />
+      <hr></hr>
+      <div className="resources">
+        <h1>Resources</h1>
+        <a href="https://www.nationalgeographic.org/encyclopedia/global-warming/">What is Global Warming?</a>
+        <a href="https://climate.nasa.gov/resources/global-warming/">Global Warming vs. Climate Change</a>
+        <a href="https://www.ucsusa.org/">Union Of Concerned Scientists</a>
+      </div>
+
     </div>
 
     )
