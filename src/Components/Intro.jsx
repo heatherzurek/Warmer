3bab4ad261ab57691c0f52b2b775db22b17c46bc
Heather Zurek <heather.zurek@gmail.com>
2019-07-26 10:21:31 -0700

    edit intro component

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -5,12 +5,9 @@ export default function Intro(props) {
 
       }
     return (
-        <div style={divStyles}>
-        <p>Earth’s climate is now changing faster than at any point in the history of modern civilization, primarily as a result of human activities. Global climate change has already resulted in a wide range of impacts across every region of the country and many sectors of the economy that are expected to grow in the coming decades.</p>
-        <p>
-        Thousands of studies conducted by researchers around the world have documented increases in temperature at Earth’s surface, as well as in the atmosphere and oceans. Many other aspects of global climate are changing as well. Human activities, especially emissions of heat-trapping greenhouse gases from fossil fuel combustion, deforestation, and land-use change, are the primary driver of the climate changes observed in the industrial era.
-
-        </p>
-        </div>
+      <div style={divStyles}>
+        <p>Our Earth's climate is now changing faster than at any point in the history of modern civilization. This is primarily as a result of human activities. Global cliate change has already resulted in a wide range of impacts across every region of the country.</p>
+        <p>Thousands of studies conducted by researchers around the world have documented increases in temperature at Earth’s surface, as well as in the atmosphere and oceans. Many other aspects of global climate are changing as well. Human activities, especially emissions of heat-trapping greenhouse gases from fossil fuel combustion, deforestation, and land-use change, are the primary driver of the climate changes observed in the industrial era.</p>
+      </div>
     )
 };
